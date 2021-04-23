4e42c74adf41290e3237259969b0d82b51f2ce48
Jhonata <gutemberg.jhonata@gmail.com>
2021-04-23 13:48:24 -0300

    style(player): disable previous and next buttons when episode is first or last

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -18,6 +18,8 @@ type PlayerContextData = {
   setPlayingState: (state: boolean) => void;
   playNext: () => void;
   playPrevious: () => void;
+  hasNext: boolean;
+  hasPrevious: boolean;
 };
 
 type PlayerContextProviderProps = {
@@ -51,16 +53,17 @@ export function PlayerProvider({ children }: PlayerContextProviderProps) {
     setIsPlaying(state);
   }
 
-  function playNext() {
-    const nextEpisodeIndex = currentEpisodeIndex + 1;
+  const hasPrevious = currentEpisodeIndex > 0;
+  const hasNext = currentEpisodeIndex + 1 < episodes.length;
 
-    if (nextEpisodeIndex < episodes.length) {
-      setCurrentEpisodeIndex(nextEpisodeIndex);
+  function playNext() {
+    if (hasNext) {
+      setCurrentEpisodeIndex(currentEpisodeIndex + 1);
     }
   }
 
   function playPrevious() {
-    if (currentEpisodeIndex > 0) {
+    if (hasPrevious) {
       setCurrentEpisodeIndex(currentEpisodeIndex - 1);
     }
   }
@@ -77,6 +80,8 @@ export function PlayerProvider({ children }: PlayerContextProviderProps) {
         setPlayingState,
         playNext,
         playPrevious,
+        hasNext,
+        hasPrevious,
       }}
     >
       {children}
